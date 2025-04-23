2f1d1a2c335e301f6b33106640ccb05a2b8f0387
skbennett <shaneunited1@gmail.com>
2025-04-23 16:55:43 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ const predictMatchup = async (team1, team2) => {
   const fixedTeam2 = normalizeTeamName(team2);
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/predict", {
+    const res = await fetch("http://3.230.0.248:8000/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ team1: fixedTeam1, team2: fixedTeam2 }),
